cb5c4e9e28e64da0012eb386c846241fb1cf3d10
Avinash kumar pandey <105911954+avinash-kr-pandey@users.noreply.github.com>
2025-08-24 01:54:17 +0530

    new code

---
+++ src/app/courses/digital-risk-&-compliance/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="bg-white text-gray-900 h-screen">
+      digital-risk-&-compliance
+    </div>
+  );
+}
+
+export default page
---
+++ src/app/courses/esg-&-sustainability/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="bg-white text-gray-900 h-screen">esg-&-sustainability</div>
+  );
+}
+
+export default page
